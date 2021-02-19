43d8325f4663bff4e0e86094d20a72dbdcd363c0
EmmaILH <79332590+EmmaILH@users.noreply.github.com>
2021-02-19 19:57:53 +0100

    mise a jour

---
+++ beta.palgania/include/wget.open.js
@@ -0,0 +1,14 @@
+﻿
+		var tmp = null;
+		$.ajax({
+				type: "GET",
+				url: "docs/wget.inside.pal-txt",
+				error:function(msg){
+                  // message en cas d'erreur :
+                  alert( "Error !: " + msg );
+                },
+				success:function(data){
+                  // affiche le contenu du fichier dans le conteneur dédié :
+                  $('#content-wget-info').text(data);
+                }
+        });
