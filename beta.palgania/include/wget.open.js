41cfba33dd292497474d4f9a5a59d0802d0a8ecb
EmmaILH <79332590+EmmaILH@users.noreply.github.com>
2021-02-20 08:39:25 +0100

    maj 20.1

--- beta.palgania/include/wget.open.js
+++ beta.palgania/include/wget.open.js
@@ -1,15 +1,16 @@
-﻿
-		var tmp = null;
-		$.ajax({
-				type: "GET",
-				url: "docs/wget.inside.html",
-				dataType : 'html',
-				error:function(msg){
-                  // message en cas d'erreur :
-                  
-                },
-				success:function(data){
-                  // affiche le contenu du fichier dans le conteneur dédié :
-                  $('#content-wget-info').html(data);
-                }
-        });
+﻿function WGetOpenView(file,element)
+{
+	var tmp = null;
+	$.ajax({
+		type: "GET",
+		url: file,
+		dataType : 'html',
+		error:function(msg){
+			// message en cas d'erreur :               
+		},
+		success:function(data){
+			// affiche le contenu du fichier dans le conteneur dédié :
+			$(element).html(data);
+		}
+	});
+}
