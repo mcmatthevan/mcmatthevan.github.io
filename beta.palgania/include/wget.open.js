e92d46be9235c4f1f8293526ee3d05fb3b2cdb04
EmmaILH <79332590+EmmaILH@users.noreply.github.com>
2021-02-20 09:47:50 +0100

    MAJ 20.1.5

--- beta.palgania/include/wget.open.js
+++ beta.palgania/include/wget.open.js
@@ -1,12 +1,14 @@
-﻿function WGetOpenView(file,element)
+﻿function WGetOpenView(file,element,type)
 {
 	var tmp = null;
 	$.ajax({
 		type: "GET",
 		url: file,
-		dataType : 'html',
+		dataType : type,
 		error:function(msg){
-			// message en cas d'erreur :               
+			// message en cas d'erreur : 
+			htmlerror = "<span class='error red'>FL-W.GET-010: Error: W.Get['"+file+"'] introuvable !</span>"
+			$(element).html(htmlerror);             
 		},
 		success:function(data){
 			// affiche le contenu du fichier dans le conteneur dédié :
