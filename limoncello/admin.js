84db94b706989786c6a73242cd1e44bcd2c54ae7
mhelluy <matthieu.helluy@gmail.com>
2021-07-14 18:08:46 +0200

    update exec

--- limoncello/admin.js
+++ limoncello/admin.js
@@ -610,7 +610,7 @@ $(function () {
                                 adder.change(function(){
                                     if ($(this).val() !== ""){
                                         for (let i = 0, c = index.length ; i < c ; ++i){
-                                            if (index[i][0] === $("#rgn_execid"+counter+" option::selected").text()){
+                                            if (index[i][0] === $("#rgn_execid"+counter+" option:selected").text()){
                                                 $("#rgn_newtx"+counter).val(index[i][1]);
                                                 break;
                                             }
