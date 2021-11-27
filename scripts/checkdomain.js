cf377c11402e69a2a154f3bc7ff88ebafff4a173
mcmatthevan <mcmatthevan@gmail.com>
2021-11-27 19:47:13 +0100

    checkdomain update

--- scripts/checkdomain.js
+++ scripts/checkdomain.js
@@ -8,6 +8,13 @@
             }
         });
         req.send();
+    } else if (location.host == "palgania.ovh"){
+        window.onload = function(e){
+            let links = document.querySelectorAll("a");
+            for (let i = 0, c = links.length ; i < c ; i++){
+                links[i].href = links[i].href.replace(/mcmatthevan\.github\.io/g,"palgania.ovh");
+            }
+        };
     }
 
 })();
