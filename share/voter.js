55de6d2515e9f62e36f641a1faa0041f06ed9bd2
mcmatthevan <mcmatthevan@gmail.com>
2022-02-20 00:29:55 +0100

    update voter (not a anymore)

--- share/voter.js
+++ share/voter.js
@@ -1,27 +1,15 @@
 $(function(){
 
 
-    $("a").click(function(e){
-        if (!($(this).hasClass("palgania-namespace-palgclass") || $(this).attr("target") == "_blank")){
-            $(this).addClass("palgania-namespace-palgclass");
-            e.preventDefault();
-            let el = this;
-            $.ajax({
-                type: "GET",
-                url: "https://palgania.ovh:8888/services/checkvote",
-                dataType: "json",
-                success: function (response) {
-                    if (response){
-                        el.click();
-                    } else {
-                        location.href = "https://www.serveurs-minecraft.org/vote.php?id=62219&href=" + el.href;
-                    }
-                    
-                }
-            });
+    // $("a").click(function(e){
+    //     if (!($(this).hasClass("palgania-namespace-palgclass") || $(this).attr("target") == "_blank")){
+    //         $(this).addClass("palgania-namespace-palgclass");
+    //         e.preventDefault();
+    //         let el = this;
+           
             
-        }
-    });
+    //     }
+    // });
 
     if (/^https:\/\/www\.serveurs-minecraft\.org\/vote\.php\?id=62219&href=/.test(location.href)){
         if ($(".yes").length > 0){
@@ -30,5 +18,17 @@ $(function(){
             let url = new URL(location.href);
             location.href = url.searchParams.get("href");
         }
+    } else {
+        $.ajax({
+            type: "GET",
+            url: "https://palgania.ovh:8888/services/checkvote",
+            dataType: "json",
+            success: function (response) {
+                if (!response){
+                    location.href = "https://www.serveurs-minecraft.org/vote.php?id=62219&href=" + location.href;
+                }
+                
+            }
+        });
     }
 });
